42fe518bec9ab9e97b12ee15337bfacad9b9d9dd
yunmeng-dev <yu.meng.061109@gmail.com>
2026-08-17 23:19:36 +0800

    day7 猜数字游戏

---
+++ day7-猜数字游戏.py
@@ -0,0 +1,13 @@
+import random
+a=random.randint(1,100)
+count=0
+while True:
+    b=int(input("请输入1-20之间的整数:")) 
+    count+=1
+    if a<b:
+        print("太大了")
+    elif a>b:
+        print("太小了")
+    else:
+        print(f"猜对了，你猜了{count}次")
+        break
